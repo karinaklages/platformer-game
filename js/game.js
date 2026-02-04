49633db35828afc82a353538a5ae397283205a94
Karina Klages <karinaklages@web.de>
2026-02-04 14:10:46 +0100

    feat: first images in canvas implemented

--- js/game.js
+++ js/game.js
@@ -1,8 +1,14 @@
 let canvas;
-let character = new Image();
+let ctx;
+let characterKnight = new Image();
 
 
 function init() {
     canvas = document.getElementById("canvas");
-    character.src = "../images/character.png";
+    ctx = canvas.getContext("2d");
+    characterKnight.src = "../img/knight/walk1.png";
+
+    setTimeout(function() {
+        ctx.drawImage(characterKnight, 50, 50, 100, 100);
+    }, 500);
 }
