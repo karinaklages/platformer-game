bbbd7e147214ceb706757325cab51dda35feebdc
Karina Klages <karinaklages@web.de>
2026-02-20 12:18:41 +0100

    feat: adjustments to implement the status bar

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,3 +1,39 @@
 class StatusBar {
+    percentage = 100;
 
+    IMAGES_COIN_BAR = [
+
+    ];
+
+    constructor() {
+        super();
+        this.loadimages(this.IMAGES_COIN_BAR);
+        this.x = 50;
+        this.y = 10;
+        // this.width = ;
+        // this.height = ;
+        this.setPercentage(100);
+    }
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.IMAGES_COIN_BAR[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
+    }
+
+    resolveImageIndex() {
+        if(this.percentage == 100) {
+            return 5;
+        } else if(this.percentage > 80) {
+            return 4;
+        } else if(this.percentage > 60) {
+            return 3;
+        } else if(this.percentage > 40) {
+            return 2;
+        } else if(this.percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,7 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
+    statusBar = new StatusBar();
 
     constructor (canvas, keyboard) {
         this.ctx = canvas.getContext("2d");
@@ -24,6 +25,9 @@ class World {
         this.addObjectsToMap(this.level.flyingTiles);
         this.addObjectsToMap(this.level.natureObjects);
         this.addToMap(this.character);
+        this.ctx.translate(-this.camera_x, 0);
+        this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.level.enemies);
         this.ctx.translate(-this.camera_x, 0);
         requestAnimationFrame(() => this.draw());
@@ -38,6 +42,7 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if(this.character.isColliding(enemy)) {
                     this.character.hit();
+                    this.statusBar.setPercentage(this.character.energy);
                     // console.log("Collision with character, energy", this.character.energy)
                 }
             });
