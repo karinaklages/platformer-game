624b0e68ea31e0fd68d5b0c6cf1bd514e4b42a2a
Karina Klages <karinaklages@web.de>
2026-03-09 18:05:25 +0100

    fix: status bars updated for better counting

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -45,13 +45,13 @@ class StatusBar extends DrawableObject {
         const path = this.images[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
-
+    
     resolveImageIndex() {
-        if (this.percentage == 100) return 5;
-        if (this.percentage > 80) return 4;
-        if (this.percentage > 60) return 3;
-        if (this.percentage > 40) return 2;
-        if (this.percentage > 20) return 1;
+        if (this.percentage >= 100) return 5;
+        if (this.percentage >= 80) return 4;
+        if (this.percentage >= 60) return 3;
+        if (this.percentage >= 40) return 2;
+        if (this.percentage >= 20) return 1;
         return 0;
     }
 }
